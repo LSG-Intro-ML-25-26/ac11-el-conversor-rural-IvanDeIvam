a499526e1ca3d1c02ada2930826ecc34ed79f8e2
Ivan Vallejo <157355377+IvanDeIvam@users.noreply.github.com>
2025-12-28 20:27:41 +0100

    trade wood per products

--- main.py
+++ main.py
@@ -1,141 +1,162 @@
+#Conversions per llenya
+WOOD_PER_CHICKEN = 6
+WOOD_PER_KG_POTATO = 1.33 # 2 / 1.5
+WOOD_PER_GOAT = 5
+WOOD_PER_EGG = 0.25 # 3 / 12
+WOOD_PER_HORSE = 12
+
 NPC = SpriteKind.create()
 
+#Variables
 npcs: List[Sprite] = []
+npc_to_talk: Sprite = None
+tree_to_chop: Sprite = None
+trees: List[Sprite] = []
+wood = 0
+MAX_TREES = 10
+TREE_SPAWN_TIME = 500
+nena: Sprite = None
+y2 = 0
+x2 = 0
+tree3: Sprite = None
+
+tiles.set_current_tilemap(tilemap("""nivel"""))
+nena = sprites.create(assets.image("""nena-front"""), SpriteKind.player)
+scene.camera_follow_sprite(nena)
+controller.move_sprite(nena)
+
+#Mètodes d'intercanvi
+
+def perform_exchange(product_name: str, wood_price: float, is_animal: bool):
+    global wood
+    quantity = game.ask_for_number("Quants " + product_name + " vols?", 3)
+    
+    if quantity <= 0:
+        game.show_long_text("Error: Ha de ser una quantitat positiva!", DialogLayout.BOTTOM)
+        return
+
+    if is_animal and quantity % 1 != 0:
+        game.show_long_text("Error: Només animals sencers!", DialogLayout.BOTTOM)
+        return
+
+    total_cost = Math.round_with_precision(quantity * wood_price, 2)
+
+    if wood >= total_cost:
+        wood -= total_cost
+        wood = Math.round_with_precision(wood, 2)
+        game.show_long_text("Has intercanviat " + str(total_cost) + "kg de llenya per " + str(quantity) + " " + product_name, DialogLayout.BOTTOM)
+    else:
+        game.show_long_text("No tens suficient llenya! Necessites " + str(total_cost) + "kg", DialogLayout.BOTTOM)
+
+def open_market_menu():
+    game.show_long_text("Mercat: 1.Gallines, 2.Kg de Patates, 3.Cabres, 4.Ous, 5.Caballs, 6.Exit", DialogLayout.BOTTOM)
+    selection = game.ask_for_string("Número de producte (1-6):", 1)
+    
+    if selection == "1":
+        perform_exchange("Gallines", WOOD_PER_CHICKEN, True)
+    elif selection == "2":
+        perform_exchange("kg de Patates", WOOD_PER_KG_POTATO, False)
+    elif selection == "3":
+        perform_exchange("Cabres", WOOD_PER_GOAT, True)
+    elif selection == "4":
+        perform_exchange("Ous", WOOD_PER_EGG, False)
+    elif selection == "5":
+        perform_exchange("Caballs", WOOD_PER_HORSE, True)
+    elif selection == "6":
+        game.show_long_text("Adeu!", DialogLayout.BOTTOM)
+
+#Mètodes de NPCs
 
 def spawn_npcs():
     global npcs
-    positions = [(72, 232), (432, 232), (72, 432), (432, 432)]
-    for pos in positions:
-        npc = sprites.create(assets.image("""
-            npc-front
-        """), NPC)
-        npc.set_position(pos[0], pos[1])
-        npcs.append(npc)
-
-npc_to_talk: Sprite = None
+    pos_x = [72, 432, 72, 432]
+    pos_y = [232, 232, 432, 432]
+    
+    for i in range(4):
+        new_npc = sprites.create(assets.image("""npc-front"""), NPC)
+        new_npc.set_position(pos_x[i], pos_y[i])
+        npcs.append(new_npc)
 
 def check_npc_overlap():
     global npc_to_talk
-    overlapping = False
-    for npc in npcs:
-        if nena.overlaps_with(npc):
-            npc_to_talk = npc
-            overlapping = True
+    npc_to_talk = None
+    for npc_item in npcs:
+        if nena.overlaps_with(npc_item):
+            npc_to_talk = npc_item
             break
-    if not overlapping:
-        npc_to_talk = None
-
-game.on_update(check_npc_overlap)
 
 def on_b_pressed():
     if npc_to_talk:
-        game.show_long_text("Hola! Puc intercanviar llenya per productes o viceversa.", DialogLayout.BOTTOM)
+        open_market_menu()
 
 controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
 
-def on_down_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-down
-            """),
-        500,
-        False)
-controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
-
-def chop_tree(tree: Sprite):
-    global wood
-    wood += 2
-    remove_tree_from_list(tree)
-    tree.destroy()
-    game.show_long_text("Has aconseguit 2kg de llenya, ara en tens " + ("" + str(wood)) + "kg",
-        DialogLayout.BOTTOM)
-
-def on_right_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-right
-            """),
-        500,
-        False)
-controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
-
-def on_left_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-left
-            """),
-        500,
-        False)
-controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
-
-def on_a_pressed():
-    if tree_to_chop:
-        chop_tree(tree_to_chop)
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
-
-def on_up_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-up
-            """),
-        500,
-        False)
-controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+#Mètodes d'arbres i llenya
 
 def spawn_tree():
     global tree3, x2, y2
     if len(trees) >= MAX_TREES:
         return
-    tree3 = sprites.create(assets.image("""
-        tree
-        """), SpriteKind.food)
+    tree3 = sprites.create(assets.image("""tree"""), SpriteKind.food)
     x2 = randint(16, scene.screen_width() - 16)
     y2 = randint(16, scene.screen_height() - 16)
     tree3.set_position(x2, y2)
     trees.append(tree3)
 
-def remove_tree_from_list(tree2: Sprite):
-    i = 0
-    while i <= len(trees) - 1:
-        if trees[i] == tree2:
-            trees.remove_at(i)
+def remove_tree_from_list(tree_to_remove: Sprite):
+    idx = 0
+    while idx < len(trees):
+        if trees[idx] == tree_to_remove:
+            trees.remove_at(idx)
             return
-        i += 1
+        idx += 1
+
+def chop_tree(tree_target: Sprite):
+    global wood
+    wood += 2
+    remove_tree_from_list(tree_target)
+    tree_target.destroy()
+    game.show_long_text("Has aconseguit 2kg de llenya, ara en tens " + ("" + str(wood)) + "kg", DialogLayout.BOTTOM)
 
 def check_tree_overlap():
     global tree_to_chop
-    overlapping = False
-    for tree in trees:
-        if nena.overlaps_with(tree):
-            tree_to_chop = tree
-            overlapping = True
+    tree_to_chop = None
+    for t in trees:
+        if nena.overlaps_with(t):
+            tree_to_chop = t
             break
-    if not overlapping:
-        tree_to_chop = None
 
-game.on_update(check_tree_overlap)
+def on_a_pressed():
+    if tree_to_chop:
+        chop_tree(tree_to_chop)
 
-y2 = 0
-x2 = 0
-tree3: Sprite = None
-trees: List[Sprite] = []
-tree_to_chop: Sprite = None
-wood = 0
-tree32: Sprite = None
-MAX_TREES = 0
-nena: Sprite = None
-TREE_SPAWN_TIME = 10000
-tiles.set_current_tilemap(tilemap("""
-    nivel
-    """))
-nena = sprites.create(assets.image("""
-    nena-front
-    """), SpriteKind.player)
-scene.camera_follow_sprite(nena)
-controller.move_sprite(nena)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+#Mètodes de botons
+
+def on_down_pressed():
+    animation.run_image_animation(nena, assets.animation("""nena-animation-down"""), 500, False)
+controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
+
+def on_right_pressed():
+    animation.run_image_animation(nena, assets.animation("""nena-animation-right"""), 500, False)
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def on_left_pressed():
+    animation.run_image_animation(nena, assets.animation("""nena-animation-left"""), 500, False)
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def on_up_pressed():
+    animation.run_image_animation(nena, assets.animation("""nena-animation-up"""), 500, False)
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
+#Altes mètodes (start, update etc)
+
+game.on_update_interval(TREE_SPAWN_TIME, spawn_tree)
+
+def on_game_update():
+    check_npc_overlap()
+    check_tree_overlap()
+game.on_update(on_game_update)
+
 spawn_npcs()
-MAX_TREES = 10
-
-def on_update_interval():
-    spawn_tree()
-game.on_update_interval(TREE_SPAWN_TIME, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,138 +1,170 @@
+// Conversions per llenya
+let WOOD_PER_CHICKEN = 6
+let WOOD_PER_KG_POTATO = 1.33
+//  2 / 1.5
+let WOOD_PER_GOAT = 5
+let WOOD_PER_EGG = 0.25
+//  3 / 12
+let WOOD_PER_HORSE = 12
 let NPC = SpriteKind.create()
+// Variables
 let npcs : Sprite[] = []
-function spawn_npcs() {
-    let npc: Sprite;
+let npc_to_talk : Sprite = null
+let tree_to_chop : Sprite = null
+let trees : Sprite[] = []
+let wood = 0
+let MAX_TREES = 10
+let TREE_SPAWN_TIME = 500
+let nena : Sprite = null
+let y2 = 0
+let x2 = 0
+let tree3 : Sprite = null
+tiles.setCurrentTilemap(tilemap`nivel`)
+nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+scene.cameraFollowSprite(nena)
+controller.moveSprite(nena)
+// Mètodes d'intercanvi
+function perform_exchange(product_name: string, wood_price: number, is_animal: boolean) {
     
-    let positions = [[72, 232], [432, 232], [72, 432], [432, 432]]
-    for (let pos of positions) {
-        npc = sprites.create(assets.image`
-            npc-front
-        `, NPC)
-        npc.setPosition(pos[0], pos[1])
-        npcs.push(npc)
+    let quantity = game.askForNumber("Quants " + product_name + " vols?", 3)
+    if (quantity <= 0) {
+        game.showLongText("Error: Ha de ser una quantitat positiva!", DialogLayout.Bottom)
+        return
+    }
+    
+    if (is_animal && quantity % 1 != 0) {
+        game.showLongText("Error: Només animals sencers!", DialogLayout.Bottom)
+        return
+    }
+    
+    let total_cost = Math.roundWithPrecision(quantity * wood_price, 2)
+    if (wood >= total_cost) {
+        wood -= total_cost
+        wood = Math.roundWithPrecision(wood, 2)
+        game.showLongText("Has intercanviat " + ("" + total_cost) + "kg de llenya per " + ("" + quantity) + " " + product_name, DialogLayout.Bottom)
+    } else {
+        game.showLongText("No tens suficient llenya! Necessites " + ("" + total_cost) + "kg", DialogLayout.Bottom)
+    }
+    
+}
+
+function open_market_menu() {
+    game.showLongText("Mercat: 1.Gallines, 2.Kg de Patates, 3.Cabres, 4.Ous, 5.Caballs, 6.Exit", DialogLayout.Bottom)
+    let selection = game.askForString("Número de producte (1-6):", 1)
+    if (selection == "1") {
+        perform_exchange("Gallines", WOOD_PER_CHICKEN, true)
+    } else if (selection == "2") {
+        perform_exchange("kg de Patates", WOOD_PER_KG_POTATO, false)
+    } else if (selection == "3") {
+        perform_exchange("Cabres", WOOD_PER_GOAT, true)
+    } else if (selection == "4") {
+        perform_exchange("Ous", WOOD_PER_EGG, false)
+    } else if (selection == "5") {
+        perform_exchange("Caballs", WOOD_PER_HORSE, true)
+    } else if (selection == "6") {
+        game.showLongText("Adeu!", DialogLayout.Bottom)
+    }
+    
+}
+
+// Mètodes de NPCs
+function spawn_npcs() {
+    let new_npc: Sprite;
+    
+    let pos_x = [72, 432, 72, 432]
+    let pos_y = [232, 232, 432, 432]
+    for (let i = 0; i < 4; i++) {
+        new_npc = sprites.create(assets.image`npc-front`, NPC)
+        new_npc.setPosition(pos_x[i], pos_y[i])
+        npcs.push(new_npc)
     }
 }
 
-let npc_to_talk : Sprite = null
-game.onUpdate(function check_npc_overlap() {
+function check_npc_overlap() {
     
-    let overlapping = false
-    for (let npc of npcs) {
-        if (nena.overlapsWith(npc)) {
-            npc_to_talk = npc
-            overlapping = true
+    npc_to_talk = null
+    for (let npc_item of npcs) {
+        if (nena.overlapsWith(npc_item)) {
+            npc_to_talk = npc_item
             break
         }
         
     }
-    if (!overlapping) {
-        npc_to_talk = null
-    }
-    
-})
+}
+
 controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
     if (npc_to_talk) {
-        game.showLongText("Hola! Puc intercanviar llenya per productes o viceversa.", DialogLayout.Bottom)
+        open_market_menu()
     }
     
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
-    animation.runImageAnimation(nena, assets.animation`
-            nena-animation-down
-            `, 500, false)
-})
-function chop_tree(tree: Sprite) {
+// Mètodes d'arbres i llenya
+function remove_tree_from_list(tree_to_remove: Sprite) {
+    let idx = 0
+    while (idx < trees.length) {
+        if (trees[idx] == tree_to_remove) {
+            trees.removeAt(idx)
+            return
+        }
+        
+        idx += 1
+    }
+}
+
+function chop_tree(tree_target: Sprite) {
     
     wood += 2
-    remove_tree_from_list(tree)
-    tree.destroy()
+    remove_tree_from_list(tree_target)
+    tree_target.destroy()
     game.showLongText("Has aconseguit 2kg de llenya, ara en tens " + ("" + ("" + wood)) + "kg", DialogLayout.Bottom)
 }
 
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    animation.runImageAnimation(nena, assets.animation`
-            nena-animation-right
-            `, 500, false)
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    animation.runImageAnimation(nena, assets.animation`
-            nena-animation-left
-            `, 500, false)
-})
+function check_tree_overlap() {
+    
+    tree_to_chop = null
+    for (let t of trees) {
+        if (nena.overlapsWith(t)) {
+            tree_to_chop = t
+            break
+        }
+        
+    }
+}
+
 controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
     if (tree_to_chop) {
         chop_tree(tree_to_chop)
     }
     
 })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
-    animation.runImageAnimation(nena, assets.animation`
-            nena-animation-up
-            `, 500, false)
+// Mètodes de botons
+controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
+    animation.runImageAnimation(nena, assets.animation`nena-animation-down`, 500, false)
 })
-function spawn_tree() {
+controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    animation.runImageAnimation(nena, assets.animation`nena-animation-right`, 500, false)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+    animation.runImageAnimation(nena, assets.animation`nena-animation-left`, 500, false)
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+    animation.runImageAnimation(nena, assets.animation`nena-animation-up`, 500, false)
+})
+// Altes mètodes (start, update etc)
+game.onUpdateInterval(TREE_SPAWN_TIME, function spawn_tree() {
     
     if (trees.length >= MAX_TREES) {
         return
     }
     
-    tree3 = sprites.create(assets.image`
-        tree
-        `, SpriteKind.Food)
+    tree3 = sprites.create(assets.image`tree`, SpriteKind.Food)
     x2 = randint(16, scene.screenWidth() - 16)
     y2 = randint(16, scene.screenHeight() - 16)
     tree3.setPosition(x2, y2)
     trees.push(tree3)
-}
-
-function remove_tree_from_list(tree2: Sprite) {
-    let i = 0
-    while (i <= trees.length - 1) {
-        if (trees[i] == tree2) {
-            trees.removeAt(i)
-            return
-        }
-        
-        i += 1
-    }
-}
-
-game.onUpdate(function check_tree_overlap() {
-    
-    let overlapping = false
-    for (let tree of trees) {
-        if (nena.overlapsWith(tree)) {
-            tree_to_chop = tree
-            overlapping = true
-            break
-        }
-        
-    }
-    if (!overlapping) {
-        tree_to_chop = null
-    }
-    
 })
-let y2 = 0
-let x2 = 0
-let tree3 : Sprite = null
-let trees : Sprite[] = []
-let tree_to_chop : Sprite = null
-let wood = 0
-let tree32 : Sprite = null
-let MAX_TREES = 0
-let nena : Sprite = null
-let TREE_SPAWN_TIME = 10000
-tiles.setCurrentTilemap(tilemap`
-    nivel
-    `)
-nena = sprites.create(assets.image`
-    nena-front
-    `, SpriteKind.Player)
-scene.cameraFollowSprite(nena)
-controller.moveSprite(nena)
+game.onUpdate(function on_game_update() {
+    check_npc_overlap()
+    check_tree_overlap()
+})
 spawn_npcs()
-MAX_TREES = 10
-game.onUpdateInterval(TREE_SPAWN_TIME, function on_update_interval() {
-    spawn_tree()
-})
